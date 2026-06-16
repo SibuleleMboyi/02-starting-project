6b8ccf05f81fca3641ddd18791913bad8a5cde15
sibulele10 <sibulelem2@discovery.co.za>
2026-06-16 14:41:10 +0200

    create data model

---
+++ src/app/investment-input.model.ts
@@ -0,0 +1,14 @@
+export interface InvestmentInput {
+  initialInvestment: number;
+  duration: number;
+  annualInvestment: number;
+  expectedReturn: number;
+}
+
+// OR
+// export type InvestmentInput = {
+//   initialInvestment: number;
+//   duration: number;
+//   annualInvestment: number;
+//   expectedReturn: number;
+// }
